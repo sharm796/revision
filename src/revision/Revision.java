d6a4e02af752ecd177864697ebe12b7fb0659802
sharm796 <66034081+sharm796@users.noreply.github.com>
2020-06-24 07:20:17 -0400

    Update Revision.java

--- src/revision/Revision.java
+++ src/revision/Revision.java
@@ -15,7 +15,7 @@ public class Revision {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      System.out.println("My first project");
     }
     
 }
